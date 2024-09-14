499a86e5c5ce6e9b89b541422bb7f6e12c089a84
Javithor360 <floresmejia004@gmail.com>
2024-09-13 20:32:52 -0600

    Initial commit

---
+++ src/main/java/com/nexus/NexusWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus/NexusWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
