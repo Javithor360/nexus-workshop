5fa6aa4e0120f034618fec816037bc002d8b29f1
EliDev <oscarmateoelias@gmail.com>
2024-09-24 01:48:14 -0600

    fix: Branch and rebase conflicts

--- server/src/main/java/com/nexus/server/controllers/ActivityController.java
+++ server/src/main/java/com/nexus/server/controllers/ActivityController.java
@@ -1,5 +1,23 @@
-package com.nexus.controllers;
+package com.nexus.server.controllers;
+
+import com.nexus.server.entities.Activity;
+import com.nexus.server.services.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 public class ActivityController {
+    private final ActivityService activityService;
+
+    // @Autowired
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @GetMapping
+    public List<Activity> getAllActivities() {
+        return activityService.getAllActivities();
+    }
 
 }
