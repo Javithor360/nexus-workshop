d98611df61023e96ba68c8fc47c89bb3c96688f2
yAeroo <ehamaya404@gmail.com>
2024-10-16 20:22:40 -0600

    feat: adding asynchronous data listing

--- client/src/main/resources/static/js/clientManagement.js
+++ client/src/main/resources/static/js/clientManagement.js
@@ -1,26 +1,38 @@
-$(document).ready(function() { loadUsers(); });
+$(document).ready(async function() {
+    let userToken = await getTokenRequest(); // Getting user token with async function
 
-function loadUsers() {
-    $.get("client/get", function(clients) {
-        const tbody = $('#clientsTable tbody');
-        tbody.empty();
+    await loadUsers(userToken);
 
-        let i = 1;
+});
 
-        clients.forEach(client => { // Procesar todos los usuarios
-            const newRow = `
-                <tr class="border-b-2">
-                    <td class="p-2 py-3">${i++}</td>
-                    <td class="p-2 py-3">${client.username}</td>
-                    <td class="p-2 py-3">${client.email}</td>
-                    <td class="p-2 py-3 flex flex-col md:flex-row justify-center items-center">
-                        <a class="bg-yellow-400 px-2 py-1 rounded text-black w-20 mb-2 md:mb-0 md:mr-2">Editar</a>
-                        <a class="bg-red-500 text-white px-2 py-1 rounded w-20 mt-2 md:mt-0 md:ml-2">Eliminar</a>
-                    </td>
-                </tr>
-            `;
+function loadUsers(userToken) {
+    $.ajax({
+        url: 'http://localhost:8081/users',  // Server URL
+        method: 'GET', // Method
+        headers: {
+            'Authorization': 'Bearer ' + userToken,
+            'Content-Type': 'application/json'
+        },success: function (clients) {
+            const tbody = $('#clientsTable tbody');
+            tbody.empty();
 
-            tbody.append(newRow);
-        });
+            let i = 1;
+
+            clients.forEach(client => { // Procesar todos los usuarios
+                const newRow = `
+                    <tr class="border-b-2">
+                        <td class="p-2 py-3">${i++}</td>
+                        <td class="p-2 py-3">${client.username}</td>
+                        <td class="p-2 py-3">${client.email}</td>
+                        <td class="p-2 py-3 flex flex-col md:flex-row justify-center items-center">
+                            <a class="bg-yellow-400 px-2 py-1 rounded text-black w-20 mb-2 md:mb-0 md:mr-2">Editar</a>
+                            <a class="bg-red-500 text-white px-2 py-1 rounded w-20 mt-2 md:mt-0 md:ml-2">Eliminar</a>
+                        </td>
+                    </tr>
+                `;
+
+                tbody.append(newRow);
+            });
+        }
     });
 }
