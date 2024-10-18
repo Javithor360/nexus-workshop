1f8f085a19e0868ed08b0720fb9a8d281252a66c
yAeroo <ehamaya404@gmail.com>
2024-10-18 04:03:38 -0600

    feat: new route for details of project

--- client/src/main/java/com/nexus/client/controllers/BossDashboardController.java
+++ client/src/main/java/com/nexus/client/controllers/BossDashboardController.java
@@ -1,6 +1,7 @@
 package com.nexus.client.controllers;
 
 import com.nexus.client.models.User;
+import com.nexus.client.services.ProjectService;
 import com.nexus.client.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,8 +22,12 @@ public class BossDashboardController {
     @Autowired
     private final UserService userService;
 
-    public BossDashboardController(UserService userService) {
+    @Autowired
+    private final ProjectService projectService;
+
+    public BossDashboardController(UserService userService, ProjectService projectService) {
         this.userService = userService;
+        this.projectService = projectService;
     }
 
     // ======================================== BOSS DASHBOARD ========================================
@@ -33,6 +39,7 @@ public class BossDashboardController {
         return "dashboard/boss/index";
     }
 
+    // ======================================== BOSS CLIENT MANAGEMENT ========================================
     @GetMapping("/boss/management/client")
     @PreAuthorize("hasAuthority('ACCESS_BOSS_DASHBOARD')")
     public String clientManagement(HttpSession session, Model model) {
@@ -42,14 +49,7 @@ public class BossDashboardController {
         return "dashboard/boss/clientManagement";
     }
 
-    @GetMapping("/boss/management/client/get") // === Request to get clients
-    @PreAuthorize("hasAuthority('ACCESS_BOSS_DASHBOARD')")
-    @ResponseBody
-    public List<User> clientList(HttpSession session) {
-        return userService.getUsers(session.getAttribute("token").toString());
-    }
-
-    // ======================================== Boss Projects Management ========================================
+    // ======================================== BOSS PROJECT MANAGEMENT ========================================
 
     @GetMapping("/boss/management/project")
     @PreAuthorize("hasAuthority('ACCESS_BOSS_DASHBOARD')")
@@ -60,12 +60,18 @@ public class BossDashboardController {
         return "dashboard/boss/projectManagement";
     }
 
-    @GetMapping("/boss/management/projectDetails")
+    @GetMapping("/boss/management/project/{id}")
     @PreAuthorize("hasAuthority('ACCESS_BOSS_DASHBOARD')")
-    public String projectDetails(HttpSession session, Model model) {
+    public String projectDetails(@PathVariable int id, HttpSession session, Model model) {
         User user = (User) session.getAttribute("user");
         model.addAttribute("user", user);
-        model.addAttribute("pageTitle", "Placeholder");
-        return "dashboard/boss/projectDetails";
+        model.addAttribute("pageTitle", "Project Details");
+
+        Project project = projectService.getProject(id, session.getAttribute("token").toString()); // Getting the specific project via id
+        model.addAttribute("project", project); // Adding the attribute to the view
+
+        //-> If the project doesn't exist it returns -> If everything is OK it returns the
+        //   404 error page to avoid problems           normal view
+        return  project == null ? "error/404" : "dashboard/boss/projectDetails";
     }
 }
