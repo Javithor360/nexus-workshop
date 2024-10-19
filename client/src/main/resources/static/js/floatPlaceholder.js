1364a204b796978ab6c013bd94edf4883183e399
FloppaCoder <marcelo.menjivar2006@gmail.com>
2024-10-18 21:03:32 -0600

    fixing textarea floatPlaceholder bug

--- client/src/main/resources/static/js/floatPlaceholder.js
+++ client/src/main/resources/static/js/floatPlaceholder.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    // Add class 'has-value' if the input had value after loading
-    $('.form-input input').each(function () {
+    // Add class 'has-value' if the input or textarea had value after loading
+    $('.form-input input, .form-input textarea').each(function () {
         if ($(this).val().length > 0) {
             $(this).parent().addClass('has-value');
         }
     });
 
     // Listen on inputs to add or remove the class 'has-value'
-    $('.form-input input').on('focus input', function () {
+    $('.form-input input, .form-input textarea').on('focus input', function () {
         if ($(this).val().length > 0){
             $(this).parent().addClass('has-value');
         } else {
